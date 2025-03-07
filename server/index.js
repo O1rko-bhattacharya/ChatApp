8701fb1fc68eda96d3f886a4f15bb855ac85cb55
Orko Bhattacharya <orkobhattacharya691@gmail.com>
2025-03-07 23:48:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABSE_URL;
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN],
+    origin: "https://chatapp-front-4ufk.onrender.com",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
